1f6903f89d6faa92cc74f7c289072440719b6072
punnfect <sp.kool@yahoo.com>
2024-04-26 02:22:02 -0500

    Added try catch block for input errors

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 def sort_key(dict):
     return dict["num"] 
-book_name = input("Name of the book? ")
-with open(f"books/{book_name}.txt") as f:
-    file_contents = f.read()
-
-words = len(file_contents.split())
 
 list_letters = []
 letters = {}
+book_name = input("Name of the book? ")
+
+try:
+    with open(f"books/{book_name}.txt") as f:
+        file_contents = f.read()
+except FileNotFoundError:
+    print(f"'{book_name}' not found in directory. \nExiting program... ")
+    exit()
+
+words = len(file_contents.split())
 
 for i in file_contents:
     i = i.lower()
@@ -20,8 +25,8 @@ for i in file_contents:
 for i in letters:
     list_letters.append({"letter": i, "num": letters[i]})
 
-# print(letters)
 list_letters.sort(reverse=True, key=sort_key)
+
 for i in list_letters:
     print(f"The '{i["letter"]}' character was found {i["num"]} times")
 print("--- End report ---")
